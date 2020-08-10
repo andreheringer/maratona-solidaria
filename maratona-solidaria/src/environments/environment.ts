80d41c58b81ad8728b63009106802439e0f55803
LucasStarlingdePaulaSalles <lucastarling1@gmail.com>
2020-08-10 18:26:31 -0300

    Dev: Setup auth test

--- maratona-solidaria/src/environments/environment.ts
+++ maratona-solidaria/src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  front: "http://localhost:4200",
+  apiUrl: "http://localhost:5000",
 };
 
 /*
