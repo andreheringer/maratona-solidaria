072efcb35452684e2ac9fe3953acdd940ea2805e
LucasStarlingdePaulaSalles <lucastarling1@gmail.com>
2020-08-17 19:06:29 -0300

    Dev: Navigation options

---
+++ maratona-solidaria/src/app/shared/models/menu-options.ts
@@ -0,0 +1,13 @@
+import { variable } from "@angular/compiler/src/output/output_ast";
+
+export interface Option {
+  name: string;
+  path: string;
+  active: boolean;
+}
+
+export const MENUOPTIONS: Option[] = [
+  { name: "login", path: "/loign", active: true },
+  { name: "regulamento", path: "/uploads/Regulamento.pdf", active: true },
+  { name: "sobre", path: "/about", active: true },
+];
