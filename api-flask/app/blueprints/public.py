884425fa29e47986a00716bcfdd957b2ddbe57a7
Andre Heringer <andregomesh@dcc.ufmg.br>
2020-09-04 00:53:03 -0300

    DEV(back) Add PUBLIC ENDPOINT for all equipes

--- api-flask/app/blueprints/public.py
+++ api-flask/app/blueprints/public.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 import logging
-from flask import Blueprint
+from flask import Blueprint, jsonify
+from app.models.equipe import Equipe
 
 logger = logging.getLogger(__name__)
 public_bp = Blueprint("public", __name__, url_prefix="/public")
@@ -10,6 +11,18 @@ public_bp = Blueprint("public", __name__, url_prefix="/public")
 @public_bp.route("/")
 def index():
     return "It works."
+
+
+@public_bp.route("/equipes", methods=["GET"])
+def list_donation():
+    """
+    """    
+    equipes = Equipe.query.all()
+    responseObject = {
+        "status": "success",
+        "equipes": jsonify(equipes),
+    }
+    return responseObject, 200
 
 
 @public_bp.route("/error")
