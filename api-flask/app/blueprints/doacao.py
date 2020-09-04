ad18193f11b4b7f901c728de3bcfe7271c4a4497
Andre Heringer <andregomesh@dcc.ufmg.br>
2020-09-04 00:51:17 -0300

    DEV(back) Added doacao api's

---
+++ api-flask/app/blueprints/doacao.py
@@ -0,0 +1,82 @@
+import logging
+from flask import Blueprint, request, jsonify
+
+from app.extentions import db
+from app.models.doacao import Doacao
+from app.models.colaborador import Colaborador
+from app.models.equipe import Equipe
+
+logger = logging.getLogger(__name__)
+doacao_bp = Blueprint("donation", __name__, url_prefix="/donation")
+
+
+@doacao_bp.route("/<donation_id>", methods=["GET"])
+def get_donation(donation_id):
+    """
+    """
+    donation = Doacao.query.filter_by(id=donation_id).first()
+    responseObject = {
+        "status": "success",
+        "message": "Successfully queried donation",
+        "donation": jsonify(donation),
+    }
+    return responseObject, 200
+
+
+@doacao_bp.route("/list", methods=["GET"])
+def list_donation():
+    """
+    """
+    auth_header = request.headers.get('Authorization')
+    token_or_error, status = Colaborador.parse_token(auth_header)
+    if status != 200:
+        return token_or_error, status
+    resp = Colaborador.decode_auth_token(token_or_error)
+    doacoes = Doacao.query.filter_by(colaborador_id=resp)
+    responseObject = {
+        "status": "success",
+        "message": "Successfully queried all donations",
+        "donations": jsonify(doacoes),
+    }
+    return responseObject, 200
+
+
+@doacao_bp.route("/create", methods=["POST"])
+def create_donation():
+    """
+    """
+    auth_header = request.headers.get('Authorization')
+    token_or_error, status = Colaborador.parse_token(auth_header)
+    if status != 200:
+        return token_or_error, status
+    resp = Colaborador.decode_auth_token(token_or_error)
+    colaborador = Colaborador.query.filter_by(id=resp).first()
+    equipe = Equipe.query.filter_by(id=colaborador.equipe_id).first()
+    post_data = request.get_json()
+    try:
+        donate = Doacao(
+            doacao=post_data.get("doacao"),
+            tipo=post_data.get("tipo"),
+            quantidade=post_data.get("quantidade"),
+            colaborador_id=colaborador.id,
+            data=post_data.get("data"),
+            aluno_id=post_data.get("aluno_id"),
+            observacao=post_data.get("observacao"),
+            pontuacao=post_data.get("pontuacao")
+        )
+        equipe.pontuacao += post_data.get("pontuacao")
+        db.session.add(donate)
+        db.session.add(equipe)
+        db.session.commit()
+        responseObject = {
+            "status": "success",
+            "message": "Successfully registered donation.",
+            "donate_id": donate.id,
+        }
+        return responseObject, 201
+    except Exception:
+        responseObject = {
+            "status": "fail",
+            "message": "Some error occurred. Please try again.",
+        }
+        return responseObject, 401
