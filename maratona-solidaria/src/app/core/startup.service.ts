ec0106e5889cd838d821f25af58e0742cbc14ffc
LucasStarlingdePaulaSalles <lucastarling1@gmail.com>
2020-10-09 18:27:41 -0300

    Dev(front): Admin table design adjustment.

--- maratona-solidaria/src/app/core/startup.service.ts
+++ maratona-solidaria/src/app/core/startup.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { DonationService } from '../shared/stores/donations/donations.service';
 import { StudentService } from '../shared/stores/students/students.service';
 import { TeamService } from '../shared/stores/teams/teams.service';
 
@@ -7,10 +8,11 @@ import { TeamService } from '../shared/stores/teams/teams.service';
 })
 export class StartupService {
 
-  constructor(private teamService: TeamService, private studentService: StudentService){}
+  constructor(private teamService: TeamService, private studentService: StudentService, private donationService: DonationService){}
 
   public async preloadStores(): Promise<void>{
     this.teamService.syncTeams();
     this.studentService.syncStudents();
+    this.donationService.synchDonations();
   }
 }
