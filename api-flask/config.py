cc11759ea42932667d07d944cfddf076dac3e905
Andre Heringer <andregomesh@dcc.ufmg.br>
2020-10-16 16:06:51 -0300

    Update config module

--- api-flask/config.py
+++ api-flask/config.py
@@ -9,20 +9,20 @@ class Config(object):
     CSRF_ENABLED = True
     SECRET_KEY = "\xf9'\xe4p(\xa9\x12\x1a!\x94\x8d\x1c\x99l\xc7\xb7e\xc7c\x86\x02MJ\xa0"
     BCRYPT_LOG_ROUNDS = 13
-    # Conexão com o banco não estabelicida por enquanto
-    SQLALCHEMY_DATABASE_URI = os.environ["DATABASE_URL"]
 
 
 class ProductionConfig(Config):
     DEBUG = False
-
+    SQLALCHEMY_DATABASE_URI = os.environ["DATABASE_URL"]
 
 class DevelopmentConfig(Config):
     DEVELOPMENT = True
     DEBUG = True
     BCRYPT_LOG_ROUNDS = 4
-
+    SQLALCHEMY_DATABASE_URI = os.environ["DATABASE_URL"]
 
 class TestingConfig(Config):
     TESTING = True
+    DEBUG = True
     BCRYPT_LOG_ROUNDS = 4
+    SQLALCHEMY_DATABASE_URI = "sqlite://"
