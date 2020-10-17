acbfda524822538f25e6414018e5ae4156bdb37a
yasminaraujo <ygaraujo98@gmail.com>
2020-10-16 23:50:19 -0300

    adiciona funcao que ordena times na leaderboard

--- maratona-solidaria/src/app/pages/leaderboard/leaderboard.page.ts
+++ maratona-solidaria/src/app/pages/leaderboard/leaderboard.page.ts
@@ -1,28 +1,32 @@
 import { Subscription } from 'rxjs';
 import { Team } from './../../shared/models/team';
 import { TeamService } from './../../shared/stores/teams/teams.service';
-import { Component, OnDestroy, OnInit } from "@angular/core";
-import { ActivatedRoute, Router } from "@angular/router";
-import { TEAMS } from "src/app/shared/models/team";
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { TEAMS } from 'src/app/shared/models/team';
 
 @Component({
-  selector: "app-leaderboard",
-  templateUrl: "./leaderboard.page.html",
-  styleUrls: ["./leaderboard.page.css"],
+  selector: 'app-leaderboard',
+  templateUrl: './leaderboard.page.html',
+  styleUrls: ['./leaderboard.page.css'],
 })
 export class LeaderboardComponent implements OnInit, OnDestroy {
-  public returnUrl = "";
+  public returnUrl = '';
   private routeSub: Subscription;
   private teamsSub: Subscription;
   teams: Team[];
   leaderPoints: number;
 
-  constructor(private router: Router, private route: ActivatedRoute, private teamService: TeamService) {
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private teamService: TeamService
+  ) {
     this.routeSub = this.route.queryParams.subscribe((params) => {
       if (params.returnUrl) {
         this.returnUrl = params.returnUrl;
       } else {
-        this.returnUrl = "";
+        this.returnUrl = '';
       }
     });
 
@@ -33,7 +37,9 @@ export class LeaderboardComponent implements OnInit, OnDestroy {
 
   public ngOnInit() {
     const a = this.router.url;
-    this.leaderPoints = this.teams.reduce((max, team) => max.points > team.points ? max : team).points;
+    this.leaderPoints = this.teams.reduce((max, team) =>
+      max.points > team.points ? max : team
+    ).points;
     // this.teams = this.teams.sort((t1, t2) => {
     //   if (t1.points < t2.points){
     //     return 1;
@@ -50,7 +56,13 @@ export class LeaderboardComponent implements OnInit, OnDestroy {
     this.teamsSub.unsubscribe();
   }
 
-  public getRandomColor(){
-    return "warning";
+  public getRandomColor() {
+    return 'warning';
+  }
+
+  public orderByStudentQuantity() {
+    this.teams.forEach((team) => {
+      team.points = team.points / team.size;
+    });
   }
 }
