4d007ffcbcfa531b36ed199b0231336828c60732
ingridlagares <ingridlagares@hotmail.com>
2020-10-19 10:43:57 -0300

    New tests

--- api-flask/tests/test_aluno.py
+++ api-flask/tests/test_aluno.py
@@ -5,7 +5,7 @@ from app.app import create_app
 from app.extentions import db
 from app.models.equipe import Equipe
 from app.models.colaborador import Colaborador
-from app.models.doacao import Doacao
+from app.models.aluno import Aluno
 
 @pytest.fixture
 def client():
@@ -18,49 +18,54 @@ def client():
         with test_app.app_context():
             db.create_all()
             equipe1 = Equipe(id=1, nome="Computacao", pontuacao=0)
+            equipe2 = Equipe(id=2, nome="Matematica", pontuacao=0)
             colab1 = Colaborador("Gab", "gab@test.com", 1, "1234", False)
+            colab2 = Colaborador("Matheus", "matheus@test.com", 2, "1233", False)
             db.session.add(equipe1)
             db.session.add(colab1)
+            db.session.add(equipe2)
+            db.session.add(colab2)
             db.session.commit()
 
             yield client
 
             db.drop_all()
 
-def test_donation(client):
+def test_create_aluno(client):
     rv = client.post("/auth/login", json={
-        'email': 'gab@test.com',
-        'password': '1234'
+        'email': 'matheus@test.com',
+        'password': '1233'
     })
     data = rv.get_json()
-    token = data["auth_token"]
-
-    rv = client.post("/donation/create", json={"doacao": "3Arroz", "tipo":"Arroz", "quantidade": 2, "aluno_id": 1 , "pontuacao": 30}, 
+    if data['status'] == "fail":
+        token = 0
+    else:
+        token = data['auth_token']
+    rv = client.post("/aluno/create", json={"nome": "Pedro", "matricula": 20169875, "email": "pedro@email.com" , "equipe_id": 2}, 
     headers={
         'Authorization': 'Bearer %s' % token
     })
     data = rv.get_json()
     assert data['status'] == "success"
 
-def test_invalid_user_donation(client):
+def test_non_authenticated_create_aluno(client):
     rv = client.post("/auth/login", json={
         'email': 'hacker@test.com',
-        'password': '1234'
+        'password': '1233'
     })
     data = rv.get_json()
     if data['status'] == "fail":
         token = 0
     else:
         token = data['auth_token']
-
-    rv = client.post("/donation/create", json={"doacao": "3Arroz", "tipo":"Arroz", "quantidade": 2, "aluno_id": 1 , "pontuacao": 30}, 
+    rv = client.post("/aluno/create", json={"nome": "Pedro", "matricula": 20169875, "email": "pedro@email.com" , "equipe_id": 2}, 
     headers={
         'Authorization': 'Bearer %s' % token
     })
     data = rv.get_json()
     assert data['status'] == "fail"
 
-def test_list_donation(client):
+def test_list_aluno(client):
     rv = client.post("/auth/login", json={
         'email': 'gab@test.com',
         'password': '1234'
@@ -68,31 +73,14 @@ def test_list_donation(client):
     data = rv.get_json()
     token = data["auth_token"]
 
-    rv = client.get("/donation/list", 
+    rv = client.get("/aluno/list", 
     headers={
         'Authorization': 'Bearer %s' % token
     })
     data = rv.get_json()
     assert 200, response.status_code
 
-def test_invalid_user_list_donation(client):
-    rv = client.post("/auth/login", json={
-        'email': 'hacker@test.com',
-        'password': '1234'
-    })
-    data = rv.get_json()
-    if data['status'] == "fail":
-        token = 0
-    else:
-        token = data['auth_token']
-    rv = client.get("/donation/list", 
-    headers={
-        'Authorization': 'Bearer %s' % token
-    })
-    data = rv.get_json()
-    assert 200, response.status_code
-
-def test_get_donation(client):
+def test_get_aluno(client):
     rv = client.post("/auth/login", json={
         'email': 'gab@test.com',
         'password': '1234'
@@ -100,10 +88,9 @@ def test_get_donation(client):
     data = rv.get_json()
     token = data["auth_token"]
 
-    rv = client.get("/donation/2", 
+    rv = client.get("/aluno/1", 
     headers={
         'Authorization': 'Bearer %s' % token
     })
     data = rv.get_json()
     assert 200, response.status_code
-
--- api-flask/tests/test_auth.py
+++ api-flask/tests/test_auth.py
@@ -34,6 +34,13 @@ def test_registration(client):
     data = rv.get_json()
     assert data['status'] == "success"
 
+def test_invalid_registration(client):
+    rv = client.post("/auth/registration", json={
+        'email': 'gab@test.com', 'password': 'secret', 'name': 'Teta', 'equipe_id': 1
+    })
+    data = rv.get_json()
+    assert data['status'] == "fail"
+
 def test_login(client):
     rv = client.post("/auth/login", json={
         'email': 'gab@test.com',
@@ -42,3 +49,14 @@ def test_login(client):
 
     data = rv.get_json()
     assert data['status'] == "success"
+
+def test_logout(client):
+    rv = client.post("/auth/login", json={
+        'email': 'gab@test.com',
+        'password': '1234'
+    })
+    data = rv.get_json()
+    token = data["auth_token"]
+    rv =  client.get('/auth/logout',     headers={
+        'Authorization': 'Bearer %s' % token
+    })
