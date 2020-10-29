a871fd0c51eb4b076b6d7874898f9275850a1a75
henriqueup <titepessoa@gmail.com>
2020-10-28 21:40:25 -0300

    Fix(Back): include team size on public endpoint

--- api-flask/app/blueprints/public.py
+++ api-flask/app/blueprints/public.py
@@ -3,6 +3,7 @@
 import logging
 from flask import Blueprint, jsonify
 from app.models.equipe import Equipe
+from app.models.aluno import Aluno
 
 logger = logging.getLogger(__name__)
 public_bp = Blueprint("public", __name__, url_prefix="/public")
@@ -19,7 +20,19 @@ def list_teams():
     retorna a lista publica de equipes
     """
     equipes = Equipe.query.all()
-    return jsonify(equipes), 200
+    equipesComTamanho = []
+
+    for equipe in equipes:
+        tamanhoEquipe = Aluno.query.filter_by(equipe_id=equipe.id).count()
+        equipesComTamanho.append({
+            'id': equipe.id,
+            'nome': equipe.nome,
+            'pontuacao': equipe.pontuacao,
+            'sigla': equipe.sigla,
+            'tamanho': tamanhoEquipe
+        })
+
+    return jsonify(equipesComTamanho), 200
 
 
 @public_bp.route("/classificacao", methods=["GET"])
@@ -28,7 +41,19 @@ def list_classification():
     retorna a lista de equipes ordenada pela pontuacao
     """
     equipes = Equipe.query.order_by(Equipe.pontuacao.desc()).all()
-    return jsonify(equipes), 200
+    equipesComTamanho = []
+
+    for equipe in equipes:
+        tamanhoEquipe = Aluno.query.filter_by(equipe_id=equipe.id).count()
+        equipesComTamanho.append({
+            'id': equipe.id,
+            'nome': equipe.nome,
+            'pontuacao': equipe.pontuacao,
+            'sigla': equipe.sigla,
+            'tamanho': tamanhoEquipe
+        })
+
+    return jsonify(equipesComTamanho), 200
 
 
 @public_bp.route("/error")
