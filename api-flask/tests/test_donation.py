c918e13df7042a1d4131c804eb5983462e2ee150
ingridlagares <ingridlagares@hotmail.com>
2020-11-01 22:44:55 -0300

    test confirm donation

--- api-flask/tests/test_donation.py
+++ api-flask/tests/test_donation.py
@@ -7,6 +7,7 @@ from app.models.equipe import Equipe
 from app.models.colaborador import Colaborador
 from app.models.doacao import Doacao
 
+
 @pytest.fixture
 def client():
     os.environ["DATABASE_URL"] = "sqlite://"
@@ -19,13 +20,16 @@ def client():
             db.create_all()
             equipe1 = Equipe(id=1, nome="Computacao", pontuacao=0)
             colab1 = Colaborador("Gab", "gab@test.com", 1, "1234", False)
+            colab2 = Colaborador("Admin", "admin@test.com", 1, "1234", True)
             db.session.add(equipe1)
             db.session.add(colab1)
+            db.session.add(colab2)
             db.session.commit()
 
             yield client
 
             db.drop_all()
+
 
 def test_donation(client):
     rv = client.post("/auth/login", json={
@@ -40,7 +44,40 @@ def test_donation(client):
         'Authorization': 'Bearer %s' % token
     })
     data = rv.get_json()
+
     assert data['status'] == "success"
+
+def test_confirm_donation(client):
+    rv = client.post("/auth/login", json={
+        'email': 'gab@test.com',
+        'password': '1234'
+    })
+    data = rv.get_json()
+    token = data["auth_token"]
+
+    rv = client.post("/auth/login", json={
+        'email': 'admin@test.com',
+        'password': '1234'
+    })
+    data = rv.get_json()
+    admin_token = data["auth_token"]
+
+    rv = client.post("/donation/create", json={"doacao": "3Arroz", "tipo":"Arroz", "quantidade": 2, "aluno_id": 1 , "pontuacao": 30}, 
+    headers={
+        'Authorization': 'Bearer %s' % token
+    })
+    data = rv.get_json()
+    donation_id = data['donate_id']
+
+    rv = client.post("/donation/%d/confirm" % donation_id, 
+    headers={
+        'Authorization': 'Bearer %s' % admin_token
+    })
+    data = rv.get_json()
+
+    assert 'confirmado' in data and data['confirmado']
+
+
 
 def test_invalid_user_donation(client):
     rv = client.post("/auth/login", json={
